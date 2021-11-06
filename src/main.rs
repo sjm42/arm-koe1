bec4b3bf22afeec7e6c4c7b36518ea13142e2e18
Sami J. Mäkinen <sjm@iki.fi>
2021-11-06 12:58:57 +0200

    tunk

--- src/main.rs
+++ src/main.rs
@@ -27,15 +27,41 @@ fn init_f411() -> stm32f411::Peripherals {
     let p = stm32f411::Peripherals::take().unwrap();
     let rcc = &p.RCC;
     let pa = &p.GPIOA;
+    let pc = &p.GPIOC;
 
-    // Enable GPIOA clock
-    rcc.ahb1enr.modify(|_r, w| w.gpioaen().enabled());
+    // Enable GPIOA+C clocks
+    rcc.ahb1enr.modify(|_r, w| {
+        w.gpioaen().enabled();
+        w.gpiocen().enabled();
+        w
+    });
 
-    // Enable push-pull output on PA5
-    pa.moder.modify(|_r, w| w.moder5().output());
+    // Clock outputs are on PA8 = MCO1, PC9 = MCO2
+    // so configure output pins as alternate function
+    pa.otyper.modify(|_r, w| w.ot8().push_pull());
+    pa.ospeedr.modify(|_r, w| w.ospeedr8().very_high_speed());
+    pa.moder.modify(|_r, w| w.moder8().alternate());
+    pc.otyper.modify(|_r, w| w.ot9().push_pull());
+    pc.ospeedr.modify(|_r, w| w.ospeedr9().very_high_speed());
+    pc.moder.modify(|_r, w| w.moder9().alternate());
+
+    // Enable clock outputs 1+2
+    rcc.cfgr.modify(|_r, w| {
+        w.mco1pre().div1();
+        w.mco1().hsi();
+        w.mco2pre().div1();
+        w.mco2().sysclk();
+        w
+    });
+
+    // Enable push-pull output on PA5 (LED)
     pa.otyper.modify(|_r, w| w.ot5().push_pull());
+    pa.ospeedr.modify(|_r, w| w.ospeedr5().low_speed());
+    pa.moder.modify(|_r, w| w.moder5().output());
+
     p
 }
+
 #[cfg(feature = "nucleo_f411")]
 fn set_led_f411(p: &stm32f411::Peripherals, state: bool) {
     let io_port = &p.GPIOA;
@@ -72,6 +98,7 @@ fn init_f103() -> stm32f103::Peripherals {
     });
     p
 }
+
 #[cfg(feature = "blue_pill")]
 fn set_led_f103(p: &stm32f103::Peripherals, state: bool) {
     let io_port = &p.GPIOC;
